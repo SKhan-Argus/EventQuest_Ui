4b5f094738a1b764fc65075bb0a0901a6df1f618
Sahil Khan <skhan@argusoft.com>
2023-07-13 18:34:52 +0530

    pdf maker added

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Event } from '../interface/Event';
 import { Booking } from '../interface/Booking';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
 })
-export class BookingComponent {
+export class BookingComponent implements AfterViewInit {
+  @ViewChild('content', { static: false }) el!: ElementRef;
+  title = 'Booking Bill';
+
   event: Event = {
     id: 0,
     name: '',
@@ -29,15 +34,26 @@ export class BookingComponent {
     status: '',
   };
 
+  constructor(private route: ActivatedRoute) {}
 
-  constructor(private route:ActivatedRoute){}
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+  ngAfterViewInit() {
+    this.route.queryParams.subscribe((params) => {
       this.booking = JSON.parse(params['booking']);
       this.event = JSON.parse(params['event']);
-      this.booking.totalPrice = this.booking.ticketQuantity*250;
-      // Use the booking and event objects as needed
+      this.booking.totalPrice = this.booking.ticketQuantity * 250;
+    });
+  }
+
+  generatePdf() {
+    
+    const doc = new jsPDF();
+    
+    const elementToExport = this.el.nativeElement;
+
+    html2canvas(elementToExport).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      doc.addImage(imgData, 'PNG', 10, 10, 190, 0);
+      doc.save(`eventQuestBill${this.booking.id}`);
     });
   }
 }
